e15ba2e308086fb12ceaba95389207222c0b0be9
robintschroeder <robintschroeder@gmail.com>
2019-05-12 08:05:20 -0500

    Added shell project with ios and android, modified gitignore.

---
+++ src/MSC.CM.XamSh/MSC.CM.XamSh/MSC.CM.XamSh/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MSC.CM.XamSh.Services;
+using MSC.CM.XamSh.Views;
+
+namespace MSC.CM.XamSh
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
