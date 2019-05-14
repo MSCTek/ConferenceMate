91722ef816848b5cf9e29293543080dd6910fe59
robintschroeder <robintschroeder@gmail.com>
2019-05-14 09:45:29 -0500

    wired up simple sample data to the speaker page

--- src/MSC.CM.XaSh/MSC.CM.XaSh/MSC.CM.XaSh/ViewModels/SpeakerViewModel.cs
+++ src/MSC.CM.XaSh/MSC.CM.XaSh/MSC.CM.XaSh/ViewModels/SpeakerViewModel.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
-
+using MSC.CM.Xam.ModelObj.CM;
 using Xamarin.Forms;
+using MSC.CM.Xam;
 
 namespace MSC.CM.XaSh.ViewModels
 {
     public class SpeakerViewModel : BaseViewModel
     {
+        public ObservableCollection<SessionSpeaker> Speakers { get; private set; }
+
+
         public SpeakerViewModel()
         {
             Title = "Speakers";
+            Speakers = new ObservableCollection<SessionSpeaker>();
 
-            //OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+            var sample1 = Xam.ModelData.CM.DemoSessionSpeaker.SampleSessionSpeaker00.ToModelObj();
+            sample1.User = Xam.ModelData.CM.DemoUser.SampleUser00.ToModelObj();
+            var sample2 = Xam.ModelData.CM.DemoSessionSpeaker.SampleSessionSpeaker01.ToModelObj();
+            sample2.User = Xam.ModelData.CM.DemoUser.SampleUser01.ToModelObj();
+
+            Speakers.Add(sample1);
+            Speakers.Add(sample2);
         }
 
-        //public ICommand OpenWebCommand { get; }
     }
 }
