45818b42b6fb24bdd67f66d37750457d4cf85369
Paul <Paul@msctek.com>
2019-06-14 16:42:12 -0500

    Minor patch to implement Guid-based SessionLikeId in SetSessionLikeAsync() methods.

--- src/MSC.CM.XaSh/MSC.CM.XaSh/MSC.CM.XaSh/ViewModels/SessionsByTimeViewModel.cs
+++ src/MSC.CM.XaSh/MSC.CM.XaSh/MSC.CM.XaSh/ViewModels/SessionsByTimeViewModel.cs
@@ -14,92 +14,92 @@ using MSC.CM.XaSh.Helpers;
 
 namespace MSC.CM.XaSh.ViewModels
 {
-    public class SessionsByTimeViewModel : BaseViewModel
-    {
-        private ObservableCollection<Session> _sessions;
+	public class SessionsByTimeViewModel : BaseViewModel
+	{
+		private ObservableCollection<Session> _sessions;
 
-        public SessionsByTimeViewModel(IDataStore store = null, IDataLoader loader = null)
-        {
-            DataStore = store;
-            DataLoader = loader;
-            Title = "Sessions By Time";
-            Sessions = new ObservableCollection<Session>();
-        }
+		public SessionsByTimeViewModel(IDataStore store = null, IDataLoader loader = null)
+		{
+			DataStore = store;
+			DataLoader = loader;
+			Title = "Sessions By Time";
+			Sessions = new ObservableCollection<Session>();
+		}
 
-        public RelayCommand<int> LikeCommand
-        {
-            get
-            {
-                return new RelayCommand<int>(async (sessionId) =>
-                {
-                    try
-                    {
-                        if (await DataStore.ToggleSessionLikeAsync(sessionId))
-                        {
-                            //re populate local list from sqlite
-                            Sessions = (await DataStore.GetSessionsAsync()).ToObservableCollection();
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Crashes.TrackError(ex);
-                    }
-                });
-            }
-        }
+		public RelayCommand<int> LikeCommand
+		{
+			get
+			{
+				return new RelayCommand<int>(async (sessionId) =>
+				{
+					try
+					{
+						if (await DataStore.ToggleSessionLikeAsync(sessionId))
+						{
+							//re populate local list from sqlite
+							Sessions = (await DataStore.GetSessionsAsync()).ToObservableCollection();
+						}
+					}
+					catch (Exception ex)
+					{
+						Crashes.TrackError(ex);
+					}
+				});
+			}
+		}
 
-        public ObservableCollection<Session> Sessions
-        {
-            get { return _sessions; }
-            set { Set(nameof(Sessions), ref _sessions, value); }
-        }
+		public ObservableCollection<Session> Sessions
+		{
+			get { return _sessions; }
+			set { Set(nameof(Sessions), ref _sessions, value); }
+		}
 
-        public async Task RefreshListViewData()
-        {
-            if (IsBusy) { return; }
+		public async Task RefreshListViewData()
+		{
+			if (IsBusy) { return; }
 
-            IsBusy = true;
+			IsBusy = true;
 
-            try
-            {
-                if ((Connectivity.NetworkAccess == NetworkAccess.Internet && await DataLoader.HeartbeatCheck()) || App.UseSampleDataStore)
-                {
-                    //load SQLite from API or sample data
-                    var ctUsers = await DataLoader.LoadUsersAsync();
-                    Debug.WriteLine($"Loaded {ctUsers} Users.");
-                    var ctRooms = await DataLoader.LoadRoomsAsync();
-                    Debug.WriteLine($"Loaded {ctRooms} Rooms.");
-                    var ctSessions = await DataLoader.LoadSessionsAsync();
-                    Debug.WriteLine($"Loaded {ctSessions} Sessions.");
-                    var ctSessionSpeakers = await DataLoader.LoadSessionSpeakersAsync();
-                    Debug.WriteLine($"Loaded {ctSessionSpeakers} SessionSpeakers.");
-                }
+			try
+			{
+				if ((Connectivity.NetworkAccess == NetworkAccess.Internet && await DataLoader.HeartbeatCheck()) || App.UseSampleDataStore)
+				{
+					//load SQLite from API or sample data
+					var ctUsers = await DataLoader.LoadUsersAsync();
+					Debug.WriteLine($"Loaded {ctUsers} Users.");
+					var ctRooms = await DataLoader.LoadRoomsAsync();
+					Debug.WriteLine($"Loaded {ctRooms} Rooms.");
+					var ctSessions = await DataLoader.LoadSessionsAsync();
+					Debug.WriteLine($"Loaded {ctSessions} Sessions.");
+					var ctSessionSpeakers = await DataLoader.LoadSessionSpeakersAsync();
+					Debug.WriteLine($"Loaded {ctSessionSpeakers} SessionSpeakers.");
+				}
 
-                //clear local list
-                Sessions.Clear();
+				//clear local list
+				Sessions.Clear();
 
-                //populate local list
-                var items = await DataStore.GetSessionsAsync();
-                foreach (var item in items)
-                {
-                    Sessions.Add(item);
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.StackTrace);
-                Crashes.TrackError(ex);
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
+				//populate local list
+				var items = await DataStore.GetSessionsAsync();
+				foreach (var item in items)
+				{
+					Sessions.Add(item);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.StackTrace);
+				Crashes.TrackError(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
 
-        internal async Task SetSessionLike(int sessionId, bool value)
-        {
-            DataStore.SetSessionLikeAsync(sessionId, value);
-            //DataUploader.QueueAsync()
-        }
-    }
+		internal async Task SetSessionLike(int sessionId, bool value)
+		{
+			await DataStore.SetSessionLikeAsync(sessionId, value);
+			//DataUploader.QueueAsync()
+		}
+	}
 }
